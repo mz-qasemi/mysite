693056f309cc61f4783a998893721a0fad943b61
Mz <77850237+mz-qasemi@users.noreply.github.com>
2026-08-07 14:47:11 +0430

    inclusion_tag

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from blog.models import Post,Category
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('title','author', 'counted_views', 'status','published_date')
+    list_filter = ('status','author')
+    # ordering = ['created_date']
+    search_fields = ['title','content']
+
+admin.site.register(Category)
+admin.site.register(Post,PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,18 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
 class Post(models.Model):
-    # image
-    # author
+    image = models.ImageField(upload_to='blog/',default='blog/default.jpg')
+    author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
     # tag
-    # category
+    category = models.ManyToManyField(Category)
     counted_views = models.IntegerField(default=0) 
     status = models.BooleanField(default=False)
     published_date = models.DateTimeField(null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True) 
+    class Meta:
+        ordering = ['-created_date']
 
-# select * FROM post
-# select * FROM post WHERE status = 1
+    def __str__(self):
+        return self.title
+
+    def snippets(self):
+        return self.content
