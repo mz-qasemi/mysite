3c58d4fd6e51ae2fc8b0aaabcb8dc2b1fa6bfbd5
Mz <77850237+mz-qasemi@users.noreply.github.com>
2026-08-14 13:48:27 +0430

    categories

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from blog.models import Post
+from blog.models import Category
 
 register = template.Library()
 
@@ -17,7 +18,16 @@ def function():
 def sinppet(value,arg=20):
     return value[:arg]
 
-@register.inclusion_tag("popularposts.html")
-def popularposts():
-    posts = Post.objects.filter(status=1).order_by("published_date")
+@register.inclusion_tag("blog/blog-popular-post.html")
+def latesposts(arg = 3):
+    posts = Post.objects.filter(status=1).order_by("published_date")[:arg]
     return {'posts':posts}
+
+@register.inclusion_tag("blog/blog-post-categories.html")
+def postcategories():
+    posts = Post.objects.filter(status=1)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name] = posts.filter(category=name).count()
+    return {"categories":cat_dict}
