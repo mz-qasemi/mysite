afd67fa7eeef1adc045f7f4a8cd420476cd66716
Mz <77850237+mz-qasemi@users.noreply.github.com>
2026-08-20 22:47:16 +0430

    Accessing a view to different URLs

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 # Create your views here.
-def blog_view(request):
+def blog_view(request,cat_name = None):
     posts= Post.objects.filter(status=1)
+    if cat_name:
+        posts = posts.filter(category__name = cat_name)
     context = {"posts" : posts,}
     return render(request,"blog/blog-home.html",context)
 
@@ -13,3 +15,8 @@ def blog_single(request,pid):
 
 def test(request):
     return render(request,"test.html")
+def blog_category(request,cat_name):
+    posts = Post.objects.filter(status=1)
+    posts = posts.filter(category__name = cat_name)
+    context = {"posts" : posts,}
+    return render(request,"blog/blog-home.html",context)
