32ed8b43b36032a61ba00e3f389ed277a4708db6
Mz <77850237+mz-qasemi@users.noreply.github.com>
2026-08-22 15:32:45 +0430

    edit lastes form in index page

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -20,7 +20,7 @@ def sinppet(value,arg=20):
 
 @register.inclusion_tag("blog/blog-popular-post.html")
 def latesposts(arg = 3):
-    posts = Post.objects.filter(status=1).order_by("published_date")[:arg]
+    posts = Post.objects.filter(status=1).order_by("-published_date")[:arg]
     return {'posts':posts}
 
 @register.inclusion_tag("blog/blog-post-categories.html")
@@ -31,3 +31,8 @@ def postcategories():
     for name in categories:
         cat_dict[name] = posts.filter(category=name).count()
     return {"categories":cat_dict}
+
+@register.inclusion_tag("website/index-lastes-post.html")
+def lastpost(arg = 3):
+    posts = Post.objects.filter(status=1).order_by("-published_date")[:arg]
+    return {'posts':posts}
