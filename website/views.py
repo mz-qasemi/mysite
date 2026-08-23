d50a5e4854637f8c7072f32adf1875ddd0f61d5c
Mz <77850237+mz-qasemi@users.noreply.github.com>
2026-08-23 16:21:35 +0430

    create contact form

--- website/views.py
+++ website/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from website.models import Contact
 # Create your views here.
-from django.http import HttpResponse
-from website.forms import NameForm , ContactForm
+from django.http import HttpResponse , JsonResponse ,HttpResponseRedirect
+from website.forms import NameForm , ContactForm , NewsletterForm
 
 def index_view(request):
     return render(request,"website/index.html")
@@ -11,7 +11,23 @@ def about_view(request):
     return render(request,"website/about.html")
 
 def contact_view(request):    
-    return render(request,"website/contact.html")
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+    form = ContactForm()
+    return render(request,"website/contact.html",{"form":form})
+
+def newsletter_view(request):
+    if request.method == "POST":
+        form  = NewsletterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    else:
+        return HttpResponseRedirect('/')
+
+
 
 def test_view(request):
     if request.method == "POST":
@@ -24,3 +40,6 @@ def test_view(request):
         
     form = ContactForm()
     return render(request,"test.html",{"form":form})
+
+
+
